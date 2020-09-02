0c2d0898dc4b7ce67a00479d8473b3f8b6adf4d7
Dave Smith <dave.smith@yale-nus.edu.sg>
2020-09-02 13:37:14 +0800

    Make preview of svg work.
    Add missing space and reference to `xmlns:xlink` in svg root node.

--- server/createSVG.js
+++ server/createSVG.js
@@ -201,6 +201,7 @@ async function toSVG(obj, writeable) {
   );
   writeable.write(
     '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" ' +
+    'xmlns:xlink="http://www.w3.org/1999/xlink" ' +
       'width="' +
       dim[0] +
       '" height="' +
